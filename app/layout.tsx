8aca36fae9a756c73c4e68c31acac8b3603a3528
unknown <dev.khalid.0@gmail.com>
2025-04-17 12:30:52 +0300

    added pixel to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,17 @@ export default function RootLayout({
           `}
         </Script>
         {/* End Google tag (gtag.js) */}
+
+        {/* Twitter conversion tracking base code */}
+        <Script id="twitter-conversion-tracking" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','p3ozr');
+          `}
+        </Script>
+        {/* End Twitter conversion tracking base code */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
