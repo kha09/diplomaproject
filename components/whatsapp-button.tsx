8bae019529ce11aa2a8532817eb8042a03ecaacf
unknown <dev.khalid.0@gmail.com>
2025-04-22 17:14:38 +0300

    changed about us section

--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image'; // Import the next/image component
 
 const WhatsAppButton = () => {
-  const phoneNumber = '96655295795';
+  const phoneNumber = '966552957095';
   const whatsappLink = `https://wa.me/${phoneNumber}`;
 
   return (
